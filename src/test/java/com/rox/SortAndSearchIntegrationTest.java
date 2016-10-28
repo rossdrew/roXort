ba5cc35127bf757cb88d568dec2948ddc87aba5b
rossdrew <thek1ng13@hotmail.com>
2016-10-28 10:50:41 +0100

    Integrations: Some cleanup

--- src/test/java/com/rox/SortAndSearchIntegrationTest.java
+++ src/test/java/com/rox/SortAndSearchIntegrationTest.java
@@ -3,18 +3,16 @@ package com.rox;
 import com.rox.search.LinearSearch;
 import com.rox.search.Searcher;
 import com.rox.sort.BubbleSort;
-import com.rox.sort.Sorter;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class SortAndSearchIntegrationTest {
+
     @Test
     public void testSearchingAnUnorderedList(){
         Integer[] list = new Integer[] {1,9,2,8,3,0,5,4,7,6};
-        Sorter sorter = new BubbleSort();
-        sorter.sort(list);
-        Searcher searcher = new LinearSearch(sorter.sort(list));
+        Searcher searcher = new LinearSearch(new BubbleSort().sort(list));
 
         assertEquals(searcher.search(9), 9);
         assertEquals(searcher.search(1), 1);
