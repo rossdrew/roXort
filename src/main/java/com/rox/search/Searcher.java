c01f63f9d9a1e9bec6863968105f8f37a9d7f5a4
rossdrew <thek1ng13@hotmail.com>
2016-11-28 13:35:54 +0000

    Getting rid of static analysis warning after generisizing search target

--- src/main/java/com/rox/search/Searcher.java
+++ src/main/java/com/rox/search/Searcher.java
@@ -3,6 +3,6 @@ package com.rox.search;
 /**
  * @author rossdrew
  */
-public interface Searcher<ComparableSearchTarget extends Comparable> {
+public interface Searcher<ComparableSearchTarget extends Comparable<ComparableSearchTarget>> {
     int search(ComparableSearchTarget searchTerm);
 }
