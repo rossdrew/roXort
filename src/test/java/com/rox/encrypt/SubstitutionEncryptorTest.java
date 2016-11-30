bd5daca345db8f31806d9a42abcb2001887ac02f
rossdrew <thek1ng13@hotmail.com>
2016-11-30 14:18:01 +0000

    Encr.Sub: Adding test for illegal alphabet usage, caught by test coverage

--- src/test/java/com/rox/encrypt/SubstitutionEncryptorTest.java
+++ src/test/java/com/rox/encrypt/SubstitutionEncryptorTest.java
@@ -2,8 +2,7 @@ package com.rox.encrypt;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 public class SubstitutionEncryptorTest {
     @Test
@@ -19,7 +18,22 @@ public class SubstitutionEncryptorTest {
         try {
             new SubstitutionEncryptor("A", "123");
             fail("Mismatched substitutions are invalid");
-        }catch(IllegalArgumentException e){ }
+        }catch(IllegalArgumentException e){
+            //Mismatched substitutions ("1,2,3") for alphabet ("A") expected
+        }
+    }
+
+    @Test
+    public void testUnknownAlphabetCharacter(){
+        SubstitutionEncryptor encryptor = new SubstitutionEncryptor("ABC", "123");
+
+        try {
+            encryptor.encrypt("DEF");
+
+            fail("Plaintext must be made up of Substitution alphabet");
+        }catch(IllegalArgumentException e){
+            //Plaintext ("DEF") contained characters not in the substitution index ("ABC")
+        }
     }
 
     @Test
